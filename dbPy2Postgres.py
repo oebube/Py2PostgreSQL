703935d9884544a007fca704a0f8662316b446a1
oebube <oebube@gmail.com>
2023-07-04 11:37:17 -0400

    First Commit

---
+++ dbPy2Postgres.py
@@ -0,0 +1,38 @@
+DB_HOST = "localhost"
+DB_NAME = "northwind"
+DB_USER = "postgres"
+DB_PASS = "ebube"
+
+import psycopg2
+import psycopg2.extras
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+
+    #cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    #cur.execute("CREATE TABLE student (id SERIAL PRIMARY KEY, NAME VARCHAR);") # already created, commented to avoid errors
+
+    # cur.execute("INSERT INTO student (name) VALUES(%s)", ("John",))
+    # cur.execute("INSERT INTO student (name) VALUES(%s)", ("Bobby",))
+    # cur.execute("INSERT INTO student (name) VALUES(%s)", ("Daniel",))
+    # cur.execute("INSERT INTO student (name) VALUES(%s)", ("Christina",))
+
+    # cur.execute("SELECT * FROM student;")
+    # print(cur.fetchall())
+
+with conn:
+
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        # cur.execute("SELECT * FROM student WHERE id = %s;", (3,))
+        # print(cur.fetchone()['name'])
+        # cur.execute("INSERT INTO student (name) VALUES(%s)", ("Janet",))
+
+        cur.execute("SELECT * FROM student;")
+        print(cur.fetchall())
+
+#conn.commit()
+
+# cur.close()
+
+conn.close()
